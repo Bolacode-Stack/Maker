fcf267fff6f4fb27d6e08852594cf05c104c5236
Mobolaji Ajibola Jr <bolaex@yahoo.com>
2025-11-10 11:06:48 +0100

    Maker form Revamp

--- Code/maker.js
+++ Code/maker.js
@@ -1,26 +1,39 @@
 const makerInput = document.getElementById("maker-input");
+const makerForm = document.getElementById('maker-form');
 const messageText = document.querySelector(".message-text");
 const makerButton = document.getElementById("maker-button");
 const mailRegex = /^[a-zA-Z][a-zA-Z0-9\-\_\.]+@[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}$/;
 
+let success ="Message Sent!"
+let error =  "Oops! That doesn't look like an email address";
+
 makerInput.addEventListener('input', (e)  =>  {
     let input = e.target.value;
-    makerButton.addEventListener("click", (e)  => {
+    makerForm.addEventListener("submit", (e)  => {
         e.preventDefault();
         if (input.match(mailRegex))  {
-         messageText.innerText = 'Message Sent!'
-        messageText.style.display = 'block';
-        messageText.style.color = 'green';
-         makerInput.style.border = '2px solid #3EE9E5'
+         messageText.innerText = success;
+         messageText.style.color = 'green';
+         makerInput.style.border = '2px solid #3EE9E5';
         } else if (makerInput.value == "" || !input.match(mailRegex))  {
-           messageText.style.display = 'block';
             makerInput.style.border = "2px solid #FF2965";
-            messageText.innerText = "Oops! That doesn't look like an email address"
-            messageText.style.color = "#FF2965";
+            messageText.innerText = error;
+            messageText.classList.add("error");
         }
         makerInput.value = "";
         })  
     });
+
+    const formValues = (event)  => {
+        event.preventDefault();
+
+        const formData = new FormData(makerForm);
+        const item = formData.get('item');
+
+        let values = item;
+  }
+
+    makerForm.addEventListener("submit", formValues)
 
     window.onload = ()  =>  {
         makerInput.value = "";
